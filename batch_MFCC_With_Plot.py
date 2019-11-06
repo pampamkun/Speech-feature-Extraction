02a5f380bb246763a1123eaa8a61f2d12dcf35d9
Muhammad Murah Pamuji <pamuji.izy@gmail.com>
2019-11-06 22:05:59 +0700

    Added some features
    - Add feature to extract MFCC directly to xls
    - You have to decide the name for destination directory, if directory exist, your results gonna end piling up inside that existing folder

--- batch_MFCC_With_Plot.py
+++ batch_MFCC_With_Plot.py
@@ -5,32 +5,44 @@ import librosa
 import matplotlib.pyplot as plt
 import sys
 import numpy
+import xlsxwriter
 numpy.set_printoptions(threshold=sys.maxsize)
 
 
 def main():
-	result_file = open('results/MFCC_'+Src+".txt", 'w')
+
+	result_file = open(path+'/MFCC_'+Src+".txt", 'w')
 	i = 0
+	row = 0
+	col = 0
 	for filename in os.listdir(Src):
 		name,ext = os.path.splitext(filename)
 		str_name = str(name)
 		waveform, sampling_rate = librosa.load(Src+'/'+filename)
-		hop_length = 512
+		hop_length = 1024
 		tempo, beat_frames = librosa.beat.beat_track(y = waveform, sr = sampling_rate)	
 		mfcc = librosa.feature.mfcc(y = waveform, sr = sampling_rate, hop_length = hop_length, n_mfcc=13)
+		for row, data in enumerate(mfcc):
+			worksheet.write_row(col, row, data)
+		col+=1
 		source_file = str(filename)
 		mfcc_value =str(mfcc)
 		result_file.write(source_file +" "+mfcc_value+'\n')
 		i+=1
-		print(i)
-#		print(filename," MFCC -> ", mfcc)
+		print("col =",col)
+	#	print(filename," MFCC -> ", mfcc)
 		plt.plot(mfcc)
 		plt.ylabel('mfcc')
-		plt.savefig("results/"+str_name+'.png', dpi=1000)
+		plt.savefig(path+"/"+str_name+'.png', dpi=1000)
 
-path = "results"
+path = input("Destination Folder : ")
 if os.path.exists(path)==False:
-    os.mkdir(path)
+		os.mkdir(path)
+		
+workbook = xlsxwriter.Workbook(path+"/"+"MFCC"+'.xlsx')
+worksheet = workbook.add_worksheet()
+
 if __name__ == '__main__':
     Src = input("Folder Name : ")
     main()
+workbook.close()
