df7515f34bcb257d7823f6eda2335242777f9607
Muhammad Murah Pamuji <pamuji.izy@gmail.com>
2019-11-06 16:48:25 +0700

    Fixing major issue on numpy x array
    Just noticed that numpy won't give you all array values if it is too big

--- MFCC.py
+++ MFCC.py
@@ -3,20 +3,25 @@ import pyloudnorm as pyln
 import os
 import librosa
 import matplotlib.pyplot as plt
-
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 def main():
+
 	result_file = open('results/MFCC'+fname+".txt", 'w')
 	waveform, sampling_rate = librosa.load(fname)
+	name,ext = os.path.splitext(fname)
+	str_name = str(name)
 	hop_length = 512
 	tempo, beat_frames = librosa.beat.beat_track(y = waveform, sr = sampling_rate)	
 	mfcc = librosa.feature.mfcc(y = waveform, sr = sampling_rate, hop_length = hop_length, n_mfcc=13)
 	source_file = str(fname)
 	mfcc_value =str(mfcc)
 	result_file.write(source_file +" "+mfcc_value+'\n')
-	print(fname," MFCC -> ", mfcc)
+	print(name," MFCC -> ", mfcc)
 	plt.plot(mfcc)
 	plt.ylabel('mfcc')
-	plt.savefig('plot.png', dpi=1000)
+	plt.savefig(str_name+'.png', dpi=1000)
 	plt.show()
 		
 path = "results"
