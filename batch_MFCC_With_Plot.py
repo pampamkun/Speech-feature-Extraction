82c882a5ca1702233cb97226b132d129f4376352
Muhammad Murah Pamuji <pamuji.izy@gmail.com>
2019-11-08 11:18:27 +0700

    Fixing issue#3
    Flattening array before writing to xls

--- batch_MFCC_With_Plot.py
+++ batch_MFCC_With_Plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import soundfile as sf
 import pyloudnorm as pyln
 import os
@@ -11,38 +12,35 @@ numpy.set_printoptions(threshold=sys.maxsize)
 
 
 def main():
-	print("Progressing your wish : ")
-	result_file = open(path+'/MFCC_'+Src+".txt", 'w')
-	i = 0
-	row = 0
 	col = 0
+	row = 0
+	result_file = open(path+'/MFCC_'+Src+".txt", 'w')
 	for filename in tqdm(os.listdir(Src)):
+		print("Progressing "+filename)
 		name,ext = os.path.splitext(filename)
 		str_name = str(name)
 		waveform, sampling_rate = librosa.load(Src+'/'+filename)
 		hop_length = 1024
-		tempo, beat_frames = librosa.beat.beat_track(y = waveform, sr = sampling_rate)	
-		mfcc = librosa.feature.mfcc(y = waveform, sr = sampling_rate, hop_length = hop_length, n_mfcc=13)
-		for row, data in enumerate(mfcc):
-			worksheet.write_row(col, row, data)
+		mfcc = librosa.feature.mfcc(y = waveform, sr = sampling_rate, hop_length = hop_length, n_mfcc=13)	
+		flatmfcc = mfcc.flatten()
+		worksheet.write_row(col, row, flatmfcc)
 		col+=1
 		source_file = str(filename)
 		mfcc_value =str(mfcc)
 		result_file.write(source_file +" "+mfcc_value+'\n')
-		i+=1
-	#	print(filename," MFCC -> ", mfcc) <- This line usage just for you who likes to see numbers passing by your CLI
 		plt.plot(mfcc)
 		plt.ylabel('mfcc')
 		plt.savefig(path+"/"+str_name+'.png', dpi=1000)
 
-path = input("Destination Folder : ")
-if os.path.exists(path)==False:
-		os.mkdir(path)
-		
-workbook = xlsxwriter.Workbook(path+"/"+"MFCC"+'.xlsx')
-worksheet = workbook.add_worksheet()
+
 
 if __name__ == '__main__':
-    Src = input("Folder Name : ")
+    Src = input("Dataset Folder : ")
+    path = input("Destinated Folder : ")
+    if os.path.exists(path)==False:
+    		os.mkdir(path)
+		
+    workbook = xlsxwriter.Workbook(path+"/"+"MFCC"+'.xlsx')
+    worksheet = workbook.add_worksheet()
     main()
 workbook.close()
